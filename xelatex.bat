cb93ed85c54d7f7041a847cc65bee15c2a1d7141
soberspot <soberspot@gmail.com>
2023-11-14 20:19:36 +0300

    14/11/2023_

--- xelatex.bat
+++ xelatex.bat
@@ -1,5 +1,5 @@
 @echo off
 chcp 65001
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode Заключение_эксперта.tex 
+xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode zakluchenie_experta.tex 
 pythontex Заключение_эксперта.tex
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode Заключение_эксперта.tex 
+xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode zakluchenie_experta.tex
--- xelatex_ExpertOpinion.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode ExpertOpinion.tex 
-biber.exe ExpertOpinion.tex  backend=biber
-pythontex ExpertOpinion.tex
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode ExpertOpinion.tex 
