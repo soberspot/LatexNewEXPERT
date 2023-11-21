65727a6f64d208cabe08f95cb21ba6e619b6b959
soberspot <soberspot@gmail.com>
2023-11-21 21:15:02 +0300

    Ветка test_

--- xelatex_ZaklExpert.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 65001
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode zakluchenie_experta.tex
-biber.exe zakluchenie_experta.tex  backend=biber
-pythontex zakluchenie_experta.tex
-xelatex.exe  --shell-escape  -synctex=1   -interaction=nonstopmode zakluchenie_experta.tex
